aff2d9225c30481daf7433ad5e60adbf36878c17
fatihkurtl <kurtf061@gmail.com>
2024-10-15 02:55:31 +0300

    deploy configuration added

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -7,6 +9,9 @@ urlpatterns = [
     path('hero_section/', views.HeroSectionView.as_view({'get': 'list'}), name='hero_section'),
     path('cta_section', views.CTASectionView.as_view({'get': 'list'}), name='cta_section'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
