ad59764802142ff0d7a0d2b5e55ebb53ef97f44c
fatihkurtl <kurtf061@gmail.com>
2024-10-15 02:51:40 +0300

    deploy configuration added

--- core/settings.py
+++ core/settings.py
@@ -173,9 +173,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'media/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'media']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
