cd49616c77b74070a2ec31eee279caa05c1f13e9
fatihkurtl <kurtf061@gmail.com>
2024-10-16 01:12:05 +0300

    dockerfile updated & nginx.conf added

--- core/settings.py
+++ core/settings.py
@@ -180,7 +180,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STORAGES = {
     "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
