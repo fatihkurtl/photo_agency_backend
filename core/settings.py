a73bf4249f86719082b1050bd27cf624badd7921
fatihkurtl <kurtf061@gmail.com>
2024-10-16 01:08:16 +0300

    dockerfile updated & nginx.conf added

--- core/settings.py
+++ core/settings.py
@@ -181,6 +181,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
