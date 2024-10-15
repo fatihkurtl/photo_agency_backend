dffb4e1d7d074cc1895962ffde54c2cb21afa119
fatihkurtl <kurtf061@gmail.com>
2024-10-15 05:54:49 +0300

    deploy configuration added

--- core/urls.py
+++ core/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('home/', include('home.urls')),
     path('general/', include('general.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,6 @@ from . import views
 urlpatterns = [
     path('hero_section/', views.HeroSectionView.as_view({'get': 'list'}), name='hero_section'),
     path('cta_section', views.CTASectionView.as_view({'get': 'list'}), name='cta_section'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
