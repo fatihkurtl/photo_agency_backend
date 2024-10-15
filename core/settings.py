c14383590c2b130a2ccb01c37292db62c34cfb81
fatihkurtl <kurtf061@gmail.com>
2024-10-15 05:46:02 +0300

    deploy configuration added

--- core/settings.py
+++ core/settings.py
@@ -174,9 +174,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'media/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'media']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
